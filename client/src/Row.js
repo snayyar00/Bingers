8fe42b07719af6bbff9de5523f2e64b3a765cc2c
snayyar00 <39316104+snayyar00@users.noreply.github.com>
2020-10-02 12:46:46 -0700

    Revert "done and ready to be build"
    This reverts commit 70e0d2595451c2d05f0fa2aa7786fa4122d35c7b.

--- client/src/Row.js
+++ client/src/Row.js
@@ -1,66 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
-import YouTube from 'react-youtube';
-import movieTrailer from 'movie-trailer';
-import "./Row.css";
+import './Row.css'
 
-const base_url = "https://image.tmdb.org/t/p/original"
 
-function Row({ title, fetchUrl, isLargeRow }) {
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+
+
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
-    const [trailerUrl, setTrailerUrl] = useState("");
-
     useEffect(() => {
-
         async function fetchData() {
-
             const request = await axios.get(fetchUrl);
+
             setMovies(request.data.results);
             return request;
         }
         fetchData();
     }, [fetchUrl]);
-    const opts = {
-        height: "390",
-        width: "99%",
-        playerVars: {
-            autoplay: 0,
-        }
-    }
-    const handleClick = (movie) => {
-        // console.table(movie?.title)
-        if (trailerUrl) {
-            setTrailerUrl('')
-        } else {
-            movieTrailer(movie?.title || "")
-                .then(url => {
-                    const urlParams = new URLSearchParams(new URL(url).search);
-                    setTrailerUrl(urlParams.get('v'));
-                }).catch((error) => console.log(error));
-        }
-    }
-
-
-
 
     return (
-        <div className="row">
+        <div className='row'>
             <h2>{title}</h2>
             <div className="row_posters">
-                {movies.map(movie => {
-                    return <img
+                {movies.map((movie) => (
+                    <img
                         key={movie.id}
-                        onClick={() => handleClick(movie)}
-                        className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        className="row_poster"
+                        src={`${base_url}${movie.poster_path}`}
                         alt={movie.name} />
-                })}
-            </div>
-            <div style={{ padding: "40px" }}>
-                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+                ))}
+
+
             </div>
         </div>
-    );
+
+    )
 }
 
-export default Row;
+export default Row
